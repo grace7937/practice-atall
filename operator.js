7b9cd47c72f6733b01f0476400c6c0622830facd
grace7937 <66059987+grace7937@users.noreply.github.com>
2020-06-14 21:39:47 +0900

    20200614_21:39

---
+++ operator.js
@@ -0,0 +1,3 @@
+console.log("my" + "cat");
+console.log("1" + 2);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
---
+++ variable.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let name = 'ellie';
+
+let text ='hello';
+
+console.log(text,charAt(0)); //h
+
+console.log(`value: ${text}, type: ${typerof text}`);
+text = 1;
+
+
+console.log(
+
+    const preInc
+);
+
+const value1 = false;
+const value2 = 4 < 2;
+
+//(or)
+
+console.log(`or: ${value1 || value2 || check()}`);
+
+function check(){
+    for (let i = 0; i <10; i++) {
+        console.log('imoticon');
+        
+    }
+    return true;
+    
+}
+
+
+
+
